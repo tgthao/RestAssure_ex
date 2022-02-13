e274c9e154c770d3c302a1ff8d4a95e650271c25
giangthao <thaogiang.90@gmail.com>
2022-02-13 23:17:18 +0700

    73. Multi Headers out put multiValueHeader=value1 				multiValueHeader=value2

---
+++ src/test/java/com/test/MultipleHeaders73.java
@@ -0,0 +1,31 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
+public class MultipleHeaders73 {
+
+
+@Test
+    public void multiple_headers_using_map(){
+    HashMap<String,String> headers = new HashMap<String,String>();
+    headers.put("header","value2");
+    headers.put("x-mock-match-request-headers","header");
+
+                given().
+                        baseUri("https://8f6d7436-aba9-4c1f-bc81-fdc881a11fb1.mock.pstmn.io").
+                        header("multiValueHeader","value1","value2").
+                        log().headers().
+                when().
+                        get("/get").
+                then().
+                        log().all().
+                        assertThat().
+                        statusCode(200);
+    }
+
+
+}
