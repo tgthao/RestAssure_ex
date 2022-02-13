3cfb26b96f753302c4af997912248aeb3eda8239
giangthao <thaogiang.90@gmail.com>
2022-02-13 18:43:43 +0700

    63 Finish all Hamcrest Collection Matchers

--- src/test/java/com/test/HamCrestAssert59.java
+++ src/test/java/com/test/HamCrestAssert59.java
@@ -3,7 +3,7 @@ package com.test;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.*;
 
 public class HamCrestAssert59 {
     @Test
@@ -16,7 +16,11 @@ public class HamCrestAssert59 {
             //    log().all().
                 assertThat().
                 statusCode(200).
-                body("workspaces.name",contains("My Workspace", "NexleSoft", "Test Flir", "Rest Assure", "Team Workspace"));
+                body("workspaces.name"
+                ,containsInAnyOrder("My Workspace", "NexleSoft", "Test Flir", "Rest Assure", "Team Workspace"),
+                        "workspaces.name",hasSize(5),
+                        "workspaces[0]",hasKey("id"),
+                        "workspaces[0].name",allOf(startsWith("My"),containsString("Work")));
     }
 
 
