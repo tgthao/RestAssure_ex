88cbf449c79c6a4b6eb1f17cc037801963d98494
giangthao <thaogiang.90@gmail.com>
2022-02-14 14:40:31 +0700

    78. How to reuse code request Specification

--- src/test/java/com/test/RequestSpecificationExample.java
+++ src/test/java/com/test/RequestSpecificationExample.java
@@ -2,24 +2,36 @@ package com.test;
 import io.restassured.http.Headers;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
-
-import java.util.HashMap;
-import java.util.List;
-
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.with;
+import static org.hamcrest.Matchers.equalTo;
+
 public class RequestSpecificationExample {
+    RequestSpecification requestSpecification =  with().
+            baseUri("https://api.postman.com").
+            header("X-API-KEY","PMAK-6207aa1f91e45e028abb0eea-96be721c435eb5d9df2049fffe94a58c3c");
 
     @Test
     public void validate_status_code(){
-        RequestSpecification requestSpecification = given().baseUri("https://api.postman.com").
-                header("X-API-KEY","PMAK-6207aa1f91e45e028abb0eea-96be721c435eb5d9df2049fffe94a58c3c");
-        given().spec(requestSpecification).
+                given().spec(requestSpecification).
                 when().
                     get("/workspaces").
                 then().
                     log().all().
                     assertThat().
                     statusCode(200)
+                ;
+    }
+    @Test
+    public void validate_response_body(){
+        given().spec(requestSpecification).
+                when().
+                    get("/workspaces").
+                then().
+                    log().all().
+                    assertThat().
+                    statusCode(200).
+                    body("workspaces[0].name",equalTo("My Workspace"))
                 ;
 
     }
