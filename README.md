16955369bd71711eddba1c3a5b4b2013980c0aa6
iMac2020 <thaogiang.90@gmail.com>
2022-02-16 21:52:33 +0700

    103. Multipart with post echo postman

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # RestAssure_ex
+Learn about how to create automation testing with Rest Assured
+That's good library for automation testing with APPIUM
--- src/test/java/com/test/RequestParameters.java
+++ src/test/java/com/test/RequestParameters.java
@@ -7,41 +7,55 @@ import java.util.HashMap;
 import static io.restassured.RestAssured.given;
 
 public class RequestParameters {
-            public void single_query_parameters(){
+    public void single_query_parameters() {
         given().
                 baseUri("https://postman-echo.com")
                 //.param("foo1","bar1")
                 /*Query param metter*/
-                .queryParam("foo1","bar1")
-               // .log().all()
-        .when().
+                .queryParam("foo1", "bar1")
+                // .log().all()
+                .when().
                 get("/get").
-        then().
+                then().
                 log().all().
                 assertThat().
                 statusCode(200);
 
     }
-    @Test
-            public void multiple_query_parameters(){
-        HashMap<String,String> queryHashMap = new HashMap<>();
-        queryHashMap.put("foo1","bar1");
-        queryHashMap.put("foo2","bar2");
+    public void multiple_query_parameters() {
+        HashMap<String, String> queryHashMap = new HashMap<>();
+        queryHashMap.put("foo1", "bar1");
+        queryHashMap.put("foo2", "bar2");
         given().
                 baseUri("https://postman-echo.com")
-                .param("foo1","bar1","bar2")
-                .param("foo2","bar1;bar2")
+                .param("foo1", "bar1", "bar2")
+                .param("foo2", "bar1;bar2")
                 /*Query param metter*/
-               /* .queryParam("foo1","bar1")
-                .queryParam("foo2","bar2")*/
+                /* .queryParam("foo1","bar1")
+                 .queryParam("foo2","bar2")*/
                 //.queryParams(queryHashMap)
                 .log().all()
-        .when().
-                get("/get").
-        then().
-                log().all().
-                assertThat().
-                statusCode(200);
+                .when().
+                    get("/get").
+                then().
+                    log().all().
+                    assertThat().
+                    statusCode(200);
+
+    }
+    @Test
+    public void multipart_from_data() {
+        given().
+                baseUri("https://postman-echo.com")
+                .multiPart("foo1","bar1")
+                .multiPart("foo2","bar2")
+                .log().all()
+                .when().
+                    post("/post/").
+                then().
+                    log().all().
+                    assertThat().
+                    statusCode(200);
 
     }
 
