eb6e373eb1fb09b61a2fa938c90bccf80092b1b3
iMac2020 <thaogiang.90@gmail.com>
2022-02-17 19:52:31 +0700

    113. Add code to before class for reuse code

--- src/test/java/com/test/Filters.java
+++ src/test/java/com/test/Filters.java
@@ -1,8 +1,15 @@
 package com.test;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -13,7 +20,19 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class Filters {
-    @Test
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
+    @BeforeClass
+    public  void beforeClass() throws FileNotFoundException {
+        PrintStream fileOutPutStreams = new PrintStream(new File("restAssured.log"));
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder()
+                .addFilter(new RequestLoggingFilter(fileOutPutStreams))
+                .addFilter(new ResponseLoggingFilter(fileOutPutStreams));
+        requestSpecification = requestSpecBuilder.build();
+
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecification = responseSpecBuilder.build();
+    }
     public void loggingFilter() {
         given().baseUri("https://postman-echo.com")
                 .filter(new RequestLoggingFilter(LogDetail.BODY))
@@ -24,11 +43,10 @@ public class Filters {
                 //log().all()
                 .assertThat().body(matchesJsonSchemaInClasspath("echoGetSchema.json"));
 
-    }    @Test
+    }
     public void loggingFilterToFile() throws FileNotFoundException {
-        PrintStream fileOutPutStream = new PrintStream(new File("restAssured.log"));
 
-        given()
+/*        given()
                 .baseUri("https://postman-echo.com")
                 .filter(new RequestLoggingFilter(LogDetail.BODY,fileOutPutStream))
                 .filter(new ResponseLoggingFilter(LogDetail.BODY,fileOutPutStream))
@@ -36,7 +54,17 @@ public class Filters {
                 .get("/get")
                 .then()
                 //log().all()
-                .assertThat().body(matchesJsonSchemaInClasspath("echoGetSchema.json"));
+                .assertThat().body(matchesJsonSchemaInClasspath("echoGetSchema.json"));*/
 
+    }
+    @Test
+    public void ReuseloggingFilter(){
+        given(requestSpecification)
+                .baseUri("https://postman-echo.com")
+                .when()
+                .get("/get")
+                .then()
+                .spec(responseSpecification)
+                .assertThat().body(matchesJsonSchemaInClasspath("echoGetSchema.json"));
     }
 }
