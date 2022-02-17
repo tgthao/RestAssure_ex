470f092e1b54290fe32dd2c32e833c02204dd6d6
iMac2020 <thaogiang.90@gmail.com>
2022-02-17 19:30:09 +0700

    112. Logging request to Filters.java like file

--- src/test/java/com/test/Filters.java
+++ src/test/java/com/test/Filters.java
@@ -5,16 +5,34 @@ import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class Filters {
     @Test
-    public void jsonSchemaValidation() {
+    public void loggingFilter() {
         given().baseUri("https://postman-echo.com")
                 .filter(new RequestLoggingFilter(LogDetail.BODY))
                 .filter(new ResponseLoggingFilter(LogDetail.STATUS))
                 .when()
+                .get("/get")
+                .then()
+                //log().all()
+                .assertThat().body(matchesJsonSchemaInClasspath("echoGetSchema.json"));
+
+    }    @Test
+    public void loggingFilterToFile() throws FileNotFoundException {
+        PrintStream fileOutPutStream = new PrintStream(new File("restAssured.log"));
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .filter(new RequestLoggingFilter(LogDetail.BODY,fileOutPutStream))
+                .filter(new ResponseLoggingFilter(LogDetail.BODY,fileOutPutStream))
+        .when()
                 .get("/get")
                 .then()
                 //log().all()
