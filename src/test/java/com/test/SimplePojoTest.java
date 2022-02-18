ab02d2f23f7d688997bbc003e684d494167c7549
iMac2020 <thaogiang.90@gmail.com>
2022-02-18 22:44:45 +0700

    124. Simple POJO: Serialization - Create Getter Setter method and Object Manger but not good

--- src/test/java/com/test/SimplePojoTest.java
+++ src/test/java/com/test/SimplePojoTest.java
@@ -1,45 +1,65 @@
 package com.test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rest.pojo.simple.SimplePojo;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.hamcrest.Matchers;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.responseSpecification;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class SimplePojoTest {
+    ResponseSpecification customResponeSpecification;
     @BeforeClass
     public void beforeClass(){
         RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder()
-                .setBaseUri("https://a7654ec8-0542-4bb0-9e59-4171712ef559.mock.pstmn.io")
-                .addHeader("x-mock-match-request-body","true")
-                //.addHeader(ContentType.JSON.toString(), "application/json; charset=utf-8")
-                .setContentType(ContentType.JSON)
+                .setBaseUri("https://a7654ec8-0542-4bb0-9e59-4171712ef559.mock.pstmn.io").
+               // addHeader("X-API-KEY","PMAK-6207aa1f91e45e028abb0eea-96be721c435eb5d9df2049fffe94a58c3c").
+                /*setConfig(config.encoderConfig(EncoderConfig.encoderConfig().
+                        appendDefaultContentCharsetToContentTypeIfUndefined(false))).*/
+                        setContentType("application/json; charset=utf-8")
                 .log(LogDetail.ALL);
         RestAssured.requestSpecification = requestSpecBuilder.build();
 
         ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder().
                 expectStatusCode(200).
-                expectContentType(ContentType.JSON).log(LogDetail.ALL);
-        RestAssured.responseSpecification = responseSpecBuilder.build();
+                expectContentType(ContentType.JSON).
+                log(LogDetail.ALL);
+        customResponeSpecification = responseSpecBuilder.build();
     }
-
     @Test
-    public void simple_pojo_example(){
-        SimplePojo simplePojo = new SimplePojo("value1","value2");
-        simplePojo.getKey1();
-        given().
-                body(simplePojo).
-                post("/postPOJO").
-                then().spec(responseSpecification)
-                .assertThat().body("key1",equalTo("value1"),
-                        "key2",equalTo("value2"));
+    public void simple_pojo_example() throws JsonProcessingException {
+        SimplePojo simplePojo = new SimplePojo("value1", "value2");
+
+        SimplePojo deserializedPojo = given()
+                .body(simplePojo)
+        .when().
+                    post("/postPOJO").
+        then().
+                    spec(customResponeSpecification)
+                .extract().
+                response()
+                       .as(SimplePojo.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String deserializedPojoStr = objectMapper.writeValueAsString(deserializedPojo);
+        String simplePojoStr = objectMapper.writeValueAsString(simplePojo);
+        assertThat(objectMapper.readTree(deserializedPojoStr),equalTo(objectMapper.readTree(simplePojoStr)));
     }
 }
