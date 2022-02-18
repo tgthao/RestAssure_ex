62ea3d65b3b0b5920a94907dc6798f0b0b5ce636
iMac2020 <thaogiang.90@gmail.com>
2022-02-18 11:15:17 +0700

    119. Serialize Jackson Object Node to JSON Object

--- src/test/java/com/test/JacksonAPI_JSONARRAY.java
+++ src/test/java/com/test/JacksonAPI_JSONARRAY.java
@@ -2,6 +2,7 @@ package com.test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
@@ -27,8 +28,8 @@ public class JacksonAPI_JSONARRAY {
     @BeforeClass
     public void beforeClass(){
         RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder()
-                .setBaseUri("https://a7654ec8-0542-4bb0-9e59-4171712ef559.mock.pstmn.io").
-                addHeader("x-mock-match-request-body","true").
+                .setBaseUri("https://api.postman.com").
+                addHeader("X-API-KEY","PMAK-6207aa1f91e45e028abb0eea-96be721c435eb5d9df2049fffe94a58c3c").
                 /*setConfig(config.encoderConfig(EncoderConfig.encoderConfig().
                         appendDefaultContentCharsetToContentTypeIfUndefined(false))).*/
                         setContentType("application/json; charset=utf-8")
@@ -43,7 +44,7 @@ public class JacksonAPI_JSONARRAY {
     }
 
 
-    @Test
+
     public void validate_post_request_payload_as_json_array_list() throws JsonProcessingException {
         HashMap<String,String> hashMap = new HashMap<>();
         hashMap.put("id","5001");
@@ -64,5 +65,26 @@ public class JacksonAPI_JSONARRAY {
                 post("/post").
                 then().spec(customResponeSpecification)
                 .assertThat().body("msg",equalTo("success"));
+    }
+    @Test
+    public void serialize_json_using_jackson() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ObjectNode nestedObjectNode = objectMapper.createObjectNode();
+
+        nestedObjectNode.put("name","My Name API UAT Rest Assured");
+        nestedObjectNode.put("type","personal");
+        nestedObjectNode.put("description","My Name API UAT created by GT");
+
+        ObjectNode  mainObjectNode = objectMapper.createObjectNode();
+        mainObjectNode.set("workspace",nestedObjectNode);
+        String mainObjectStr = objectMapper.writeValueAsString(mainObjectNode);
+
+
+        given().
+                body(mainObjectStr).
+                when().
+                post("/workspaces").
+                then().spec(customResponeSpecification)
+                .assertThat().body("workspace.name",equalTo("My Name API UAT Rest Assured"));
     }
 }
