d5fc9fb5a7112ba5781f33307fedf9a33c6b435c
iMac2020 <thaogiang.90@gmail.com>
2022-02-19 09:26:00 +0700

    125. Workspace create workspace using POJO

---
+++ src/test/java/com/test/WorkspacePojoTest.java
@@ -0,0 +1,56 @@
+package com.test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rest.pojo.simple.SimplePojo;
+import com.rest.pojo.workspace.Workspace;
+import com.rest.pojo.workspace.WorkspaceRoot;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.matchesPattern;
+
+public class WorkspacePojoTest {
+    ResponseSpecification customResponeSpecification;
+    @BeforeClass
+    public void beforeClass(){
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder()
+                .setBaseUri("https://api.postman.com")
+                .addHeader("X-API-KEY","PMAK-6207aa1f91e45e028abb0eea-96be721c435eb5d9df2049fffe94a58c3c")
+
+        // addHeader("X-API-KEY","PMAK-6207aa1f91e45e028abb0eea-96be721c435eb5d9df2049fffe94a58c3c").
+                /*setConfig(config.encoderConfig(EncoderConfig.encoderConfig().
+                        appendDefaultContentCharsetToContentTypeIfUndefined(false))).*/
+        .setContentType("application/json; charset=utf-8")
+                .log(LogDetail.ALL);
+        RestAssured.requestSpecification = requestSpecBuilder.build();
+
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder().
+                expectStatusCode(200).
+                expectContentType(ContentType.JSON).
+                log(LogDetail.ALL);
+        customResponeSpecification = responseSpecBuilder.build();
+    }
+    @Test
+    public void simple_pojo_example() throws JsonProcessingException {
+        Workspace workspace = new Workspace("My API RestAssured 1","personal","API RestAssured created");
+        WorkspaceRoot workspaceRoot = new WorkspaceRoot(workspace);
+        WorkspaceRoot deserializedRoot = given().body(workspaceRoot)
+                .when().post("/workspaces")
+                .then().spec(customResponeSpecification)
+                .extract()
+                .response()
+                .as(WorkspaceRoot.class);
+        assertThat(deserializedRoot.getWorkspace().getName(),equalTo(workspaceRoot.getWorkspace().getName()));
+        assertThat(deserializedRoot.getWorkspace().getId(),matchesPattern("^[a-z0-9-]{36}$"));
+    }
+}
