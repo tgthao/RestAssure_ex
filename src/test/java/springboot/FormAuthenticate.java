d0085dc6b94cd9f2b3e1accd70ebae553424e442
iMac2020 <thaogiang.90@gmail.com>
2022-02-21 20:35:28 +0700

    177. Sending Cookie use Cookie builder using Cookie Builder

--- src/test/java/springboot/FormAuthenticate.java
+++ src/test/java/springboot/FormAuthenticate.java
@@ -5,6 +5,7 @@ import io.restassured.RestAssured;
 import io.restassured.authentication.FormAuthConfig;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.session.SessionFilter;
+import io.restassured.http.Cookie;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -43,7 +44,6 @@ public class FormAuthenticate {
                 .assertThat().statusCode(200)
                 .body("html.body.div.p",equalTo("This is User Profile\\Index. Only authenticated people can see this"));
     }
-    @Test
     public  void sending_cookie_flow(){
         SessionFilter sessionFilter = new SessionFilter();
         given().
@@ -60,6 +60,34 @@ public class FormAuthenticate {
         System.out.println("Session id: "+sessionFilter.getSessionId());
         given()
                 .cookie("JSESSIONID",sessionFilter.getSessionId())
+                .log().all()
+        .when()
+                .get("/profile/index")
+         .then()
+                .log().all()
+                .assertThat().statusCode(200)
+                .body("html.body.div.p",equalTo("This is User Profile\\Index. Only authenticated people can see this"));
+    }    @Test
+    public  void sending_cookie_builder(){
+        SessionFilter sessionFilter = new SessionFilter();
+        given().
+                auth().form("dan", "dan123", new FormAuthConfig("/signin",
+                        "txtUsername", "txtPassword").withAutoDetectionOfCsrf())
+                .filter(sessionFilter)
+                .log().all().
+         when().
+                get("/login")
+        .then().
+                log().all()
+                .assertThat()
+                .statusCode(200);
+        System.out.println("Session id: "+sessionFilter.getSessionId());
+        Cookie cookie = new Cookie.Builder("JSESSIONID",sessionFilter
+                .getSessionId()).setSecured(true).setHttpOnly(true)
+                        .setComment("my cookie"). build();
+
+        given()
+                .cookie(cookie)
                 .log().all()
         .when()
                 .get("/profile/index")
