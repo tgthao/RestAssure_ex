453f90af680c1e40e8f4407a42109a70636fac00
iMac2020 <thaogiang.90@gmail.com>
2022-02-21 17:40:19 +0700

    173. Automate get Profile info using CSRF .withAutoDetectionOfCsrf())

--- src/test/java/springboot/FormAuthenticate.java
+++ src/test/java/springboot/FormAuthenticate.java
@@ -4,6 +4,7 @@ package springboot;
 import io.restassured.RestAssured;
 import io.restassured.authentication.FormAuthConfig;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.session.SessionFilter;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -19,15 +20,26 @@ public class FormAuthenticate {
     }
     @Test
     public  void form_authenticate_csrf(){
+        SessionFilter sessionFilter = new SessionFilter();
         given().
                 auth().form("dan", "dan123", new FormAuthConfig("/signin",
-                        "txtUsername", "txtPassword"))
-                        .log().all().
+                        "txtUsername", "txtPassword").withAutoDetectionOfCsrf())
+                .filter(sessionFilter)
+                .log().all().
          when().
                 get("/login")
         .then().
                 log().all()
                 .assertThat()
                 .statusCode(200);
+        System.out.println("Session id: "+sessionFilter.getSessionId());
+        given()
+                .sessionId(sessionFilter.getSessionId())
+                .log().all()
+        .when()
+                .get("/profile/index")
+         .then()
+                .log().all()
+                .assertThat().statusCode(200);
     }
 }
